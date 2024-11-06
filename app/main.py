74fe7ac80e97b39c991637733dd09b45bec0d174
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2024-11-06 10:18:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
 
     @classmethod
-    def reset_people(cls):
+    def reset_people(cls) -> None:
         cls.people.clear()
 
 
 def create_person_list(people_data: list[dict]) -> list[Person]:
     Person.reset_people()
 
-    person_instances = [Person(data['name'], data['age']) for data in people_data]
+    person_instances = [Person(data["name"], data["age"]) for data in people_data]
 
     for data in people_data:
-        current_person = Person.people[data['name']]
-        spouse_role = 'wife' if 'wife' in data else 'husband'
+        current_person = Person.people[data["name"]]
+        spouse_role = "wife" if "wife" in data else "husband"
         spouse_name = data.get(spouse_role)
 
         if spouse_name:
