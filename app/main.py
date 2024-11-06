21194bf9dbbe599b5af775e8979ecec1bf9e7414
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2024-11-06 10:16:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
+    @classmethod
+    def reset_people(cls):
+        cls.people.clear()
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_data: list[dict]) -> list[Person]:
+    Person.reset_people()
+
+    person_instances = [Person(data['name'], data['age']) for data in people_data]
+
+    for data in people_data:
+        current_person = Person.people[data['name']]
+        spouse_role = 'wife' if 'wife' in data else 'husband'
+        spouse_name = data.get(spouse_role)
+
+        if spouse_name:
+            spouse_instance = Person.people.get(spouse_name)
+            setattr(current_person, spouse_role, spouse_instance)
+
+    return person_instances
